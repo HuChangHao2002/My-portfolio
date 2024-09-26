011500e6c007120affb55f2b104a94a9fd1d0010
changhao2002 <huchanghao@metabit.network>
2024-09-26 22:53:51 +0800

    design home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [
+        HomeComponent, 
+        AboutComponent, 
+        ProjectsComponent, 
+        ContactComponent , 
+        HeaderComponent,
+        RouterModule
+    ],
+    standalone: true,
 })
-export class AppComponent {
-  title = 'my-portfolio';
-}
+export class AppComponent { }
