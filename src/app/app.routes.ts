6e25083751b83bb65177d07b0965dc9c851ef511
changhao2002 <huchanghao@metabit.network>
2024-09-27 14:43:26 +0800

    route to the top of each page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
-import { Routes ,RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
-
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -13,5 +12,5 @@ export const routes: Routes = [
 ];
 
 export const AppRoutingModule = RouterModule.forRoot(routes, {
-    scrollPositionRestoration: 'top' 
+    scrollPositionRestoration: 'top'
 });
