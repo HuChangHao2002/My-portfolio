786cdce0d0e8d04575a0f6255c4f5085588c3262
changhao2002 <huchanghao@metabit.network>
2024-09-27 14:57:58 +0800

    set up reactive form in contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactForm : FormGroup;
 
+  constructor( private fb: FormBuilder ){
+    this.contactForm = this.fb.group({
+      name: ['',Validators.required],
+      email:['',[Validators.required, Validators.email]],
+      message:['',Validators.required]
+    });
+  }
+
+  onSubmit(){
+    if(this.contactForm.valid){
+      const formData = this.contactForm.value;
+      alert(`Thank you for your message, ${formData.name}! We will get back to you soon.`);
+      window.location.reload();
+    }
+  }
 }
